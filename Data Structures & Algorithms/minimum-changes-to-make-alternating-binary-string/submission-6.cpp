dbc0ee0209f886ff70b1cae5825ab0638aca1056
bao nguyen <101162947+bwinnn1@users.noreply.github.com>
2026-07-29 22:23:59 -0500

    Add: minimum-changes-to-make-alternating-binary-string - submission-6

---
+++ Data Structures & Algorithms/minimum-changes-to-make-alternating-binary-string/submission-6.cpp
@@ -0,0 +1,26 @@
+class Solution {
+   public:
+    int minOperations(string s) {
+        int counter = 0;  // counter to check how many operations
+        for (int i = 0; i < s.length(); i++) {
+            if (i % 2 == 0) {
+                if (s[i] == '0') {
+                    counter++;
+                }
+            } else {
+                if (s[i] == '1') {
+                    counter++;
+                }
+            }
+        }
+
+        //the mismatch for patter start with 0 is because 
+        //match.A + mismatch.A = length
+        //matches_with_A = mismatches_with_B
+        //substinute 1 into 2:
+        //mismatches_with_B + mismatches_A = length
+        //mismatch_with_B = length - mismatches_with_A
+        //mismatch_with_B = length - counter
+        return min(counter, (int)s.size() - counter);
+    }        
+};
