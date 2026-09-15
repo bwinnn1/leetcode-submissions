dcd1bc18e51ddf083e3ef1b336750df55d2e8b6b
bao nguyen <101162947+bwinnn1@users.noreply.github.com>
2026-09-15 12:57:20 -0500

    Add: remove-duplicates-from-sorted-array - submission-4

---
+++ Data Structures & Algorithms/remove-duplicates-from-sorted-array/submission-4.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        // use two pointers
+        // start both r and l at 1 because the first element is always unique
+        int l = 1;
+        for (int r = 1; r < nums.size(); r++) {
+            // check previous number if they are not the same then update
+            if (nums[r] != nums[r-1]) {
+                // you want to update the number at index l 
+                // assign nums[i] = nums[r] then do post-increment to move on
+                nums[l++] = nums[r];
+            }
+        }
+
+        return l;
+    }
+};
